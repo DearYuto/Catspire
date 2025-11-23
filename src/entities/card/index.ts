4add21c4e33ecb40996347a6dfed5993bfe128d5
DearYuto <reasons@kakao.com>
2025-11-23 20:24:33 +0900

    chore(cards): 아직 구현되지 않은 부분 임시 주석처리

--- src/entities/card/index.ts
+++ src/entities/card/index.ts
@@ -3,6 +3,6 @@ export { WARRIOR_CARDS } from "./model/cards.warrior";
 export { MAGE_CARDS } from "./model/cards.mage";
 export { ROGUE_CARDS } from "./model/cards.rogue";
 
-export { CardView } from "./ui/card-view";
+// export { CardView } from "./ui/card-view";
 
 export type { Card, CardEffect } from "./types";
