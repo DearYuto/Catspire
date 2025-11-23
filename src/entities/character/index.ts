164e20569c8132596d20ca9febb4c7f2d4e4543a
DearYuto <reasons@kakao.com>
2025-11-23 20:31:54 +0900

    chore(character): 미구현 임시 주석 처리

--- src/entities/character/index.ts
+++ src/entities/character/index.ts
@@ -1,5 +1,5 @@
 export { CHARACTER_INFO } from "./model/character";
 export { CharacterService } from "./model/character.service";
 export { createCharacter } from "./model/character.factory";
-export { CharacterInfo } from "./ui/character-info";
+// export { CharacterInfo } from "./ui/character-info";
 export type { Character, Buff, Debuff } from "./types";
