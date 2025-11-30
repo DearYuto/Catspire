bba14168d912845991783cb56bd1d4d3d2eddb94
DearYuto <reasons@kakao.com>
2025-11-30 19:50:24 +0900

    config: 이미지 캐싱 및 헤더 설정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    minimumCacheTTL: 31536000, // 1년
+  },
+  async headers() {
+    return [
+      {
+        source: "/_next/image(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
