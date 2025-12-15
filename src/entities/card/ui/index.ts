f3a4352414c7575d3d72a2244952777e4804fb1c
DearYuto <reasons@kakao.com>
2025-12-15 20:38:31 +0900

    chore(map): 노드 룰 수정

--- src/entities/card/ui/index.ts
+++ src/entities/card/ui/index.ts
@@ -6,3 +6,7 @@ export { CardFrame } from "./card-frame";
 
 
 
+
+
+
+
--- src/entities/map/constants/rules.ts
+++ src/entities/map/constants/rules.ts
@@ -1,6 +1,7 @@
 /**
  * 전체 층 기준 몇 퍼센트 이에 배치할지 정의
  * min 0.5, max 1.0인 경우엔 전체 층 기준 50% 이후에만 생성됨.
+ * start,end는 반드시 시작과 끝에 배치할거라 제외함
  */
 export const NODE_PLACEMENT_RULES = {
   elite: {
@@ -8,8 +9,12 @@ export const NODE_PLACEMENT_RULES = {
     max: 1.0,
   },
   boss: {
-    min: 1,
-    max: 1,
+    min: 1.0,
+    max: 1.0,
+  },
+  battle: {
+    min: 0.0,
+    max: 1.0,
   },
   // TODO: 추후 추가 예정
   // event: {
@@ -28,8 +33,4 @@ export const NODE_PLACEMENT_RULES = {
   //   min: 1,
   //   max: 1,
   // },
-  end: {
-    min: 1,
-    max: 1,
-  },
-};
+} as const;
